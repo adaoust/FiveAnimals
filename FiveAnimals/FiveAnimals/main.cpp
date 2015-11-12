0e76ee97080dd263c8469fad191779292b005bb0
Alex D'Aoust <alexdaoust@videotron.ca>
2015-11-12 10:53:33 -0500

    Initial Commit

---
+++ FiveAnimals/FiveAnimals/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FiveAnimals
+//
+//  Created by Alex D'Aoust on 2015-11-12.
+//  Copyright (c) 2015 Alexandre D'Aoust - 7206757, Maxime Bélair - 7432263. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
